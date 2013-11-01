55a86f17076f7ad55096766500475ba45ec499e9
secretchina <support@secretchina.com>
2013-11-01 12:59:31 -0500

    删除FirefoxPortable目录

--- FirefoxPortable/Data/profile/extensions/cpmanager@mozillaonline.com/pfs/content/overlay-browser.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", function() {
-  if(gPluginHandler && gPluginHandler.supportedPlugins){
-    gPluginHandler.supportedPlugins.mimetypes["application/qvod-plugin"] = "qvod";
-    gPluginHandler.supportedPlugins.plugins["qvod"] = {
-      "displayName": "Qvod plugin",
-      "installWINNT": true,
-    };
-  }
-},false);
--- FirefoxPortable/Data/profile/extensions/{1018e4d6-728f-4b20-ad56-37578a4de76b}/ipdb/metadata.js
+++
@@ -1,2 +0,0 @@
-const EXPORTED_SYMBOLS = ["IPDBmetadata"];
-const IPDBmetadata = {created:1377313225000,size:{IPv4:492738,IPv6:247072}};
